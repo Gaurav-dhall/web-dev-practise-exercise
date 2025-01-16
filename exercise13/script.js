ae40e13e0ae910b47818419c84599888ed0afb5c
gaurav <gauravdhall35@gmail.com>
2025-01-16 14:26:27 +0530

    exercise 13

--- exercise13/script.js
+++ exercise13/script.js
@@ -1,7 +1,23 @@
-function createCard(title,cName,views,monthsOld,duration,thumbnail){
-     document.querySelector(".container").insertAdjacentHTML("afterbegin"," <div></div>");
-    document.quer
+let button=document.getElementById("addcard");
+
+function createCard (title , channelName,views,monthold,duration,thumbnail){
+    console.log(title,channelName,views,monthold,duration,thumbnail);
+
 
 }
+button.addEventListener("click",()=>{
 
-createCard("Backend course","Code with harry",10000,7,"24:54","https://i.ytimg.com/vi/KtL-SQ20Q0s/hqdefault.jpg?sqp=-oaymwEmCKgBEF5IWvKriqkDGQgBFQAAiEIYAdgBAeIBCggYEAIYBjgBQAE=&rs=AOn4CLDPqs5LtUVsb50otOywVTZEmI75fg");
+   let title=prompt("enter title");
+   let channelName=prompt("enter channel name");
+   let views=prompt("enter views");
+   let monthold=prompt("enter monthold");
+   let duration=prompt("enter duration");
+   let thumbnail=prompt("enter link of thumbnail");
+
+   createCard(title,channelName,views,monthold,duration,thumbnail);
+        
+});
+
+
+
+// createCard("Introduction to backend: Sigma web dev vedio #2","CodeWithHarry" ,560000,7,"31:20","https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEnCPYBEIoBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLCKktmHuXV_macV6MoUwhKxwzXJ7w")
