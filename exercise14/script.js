a3c2a595e721a26bb1fa33823363e53b2c45f329
gaurav <gauravdhall35@gmail.com>
2025-01-23 22:43:25 +0530

    exercise 14

--- exercise14/script.js
+++ exercise14/script.js
@@ -1,24 +1,25 @@
-let line1 = document.querySelector(".line1");
-let line2 = document.querySelector(".line2");
-let line3 = document.querySelector(".line3");
-let line4 = document.querySelector(".line4");
-let line5 = document.querySelector(".line5");
+
 
 let content={
-    "line1":"Initializing Hacking...",
-    "line2":"Reading your Files...",
-    "line3":"Password files Detected...",
-    "line4":"Sending all passwords and personal files to server...",
-    "line5":"Cleaning up..."
+    "line1":"Initializing Hacking",
+    "line2":"Reading your Files",
+    "line3":"Password files Detected",
+    "line4":"Sending all passwords and personal files to server",
+    "line5":"Cleaning up"
 };
 console.log(content);
-function hacking (key,timeout){
+function hacking (key,timeout,noofiterationsofdots){
     return new Promise(resolve=>{
         setTimeout(() => {
-            key.innerHTML=`${content[key]}`;
-            console.log(typeof(key))
-            console.log(content[key]);
-            console.log(content);
+        document.querySelector(`.${key}`).innerHTML=`${content[key]}`;
+     let afterDots=   document.querySelector(`.${key}`).insertAdjacentHTML("beforeend",`<span class="dot1">.</span><span class="dot2">.</span><span class="dot3">.</span>`)
+    //  document.querySelector(".dot1").style.animationIterationCount=noofiterationsofdots;
+    //  document.querySelector(".dot2").style.animationIterationCount=noofiterationsofdots;
+    //  document.querySelector(".dot3").style.animationIterationCount=noofiterationsofdots;
+            // console.log(typeof(key))
+            // console.log(content[key]);
+            // console.log(content);
+            // console.log(key);
             
         resolve();
             
@@ -35,9 +36,10 @@ async function hackit() {
     if (Object.prototype.hasOwnProperty.call(content, key)) {
       let timeout=Math.round(Math.random()*10000);
 console.log(timeout);
+let noofiterationsofdots=Math.round(timeout/1000)/2;//2s for 1 iteration of dots therefore for timeout(in second) we need to divide by 2
 const element=content[key];
 
-await hacking(key,timeout);
+await hacking(key,timeout,noofiterationsofdots);
         
     }
 }
