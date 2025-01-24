5a92005f719492e34abaf2d24a4ee03ecd3aa97e
gaurav <gauravdhall35@gmail.com>
2025-01-24 11:12:51 +0530

    exercise 14 completed

--- exercise14/script.js
+++ exercise14/script.js
@@ -13,9 +13,12 @@ function hacking (key,timeout,noofiterationsofdots){
         setTimeout(() => {
         document.querySelector(`.${key}`).innerHTML=`${content[key]}`;
      let afterDots=   document.querySelector(`.${key}`).insertAdjacentHTML("beforeend",`<span class="dot1">.</span><span class="dot2">.</span><span class="dot3">.</span>`)
-    //  document.querySelector(".dot1").style.animationIterationCount=noofiterationsofdots;
-    //  document.querySelector(".dot2").style.animationIterationCount=noofiterationsofdots;
-    //  document.querySelector(".dot3").style.animationIterationCount=noofiterationsofdots;
+
+     const parent = document.querySelector(`.${key}`);
+    parent.querySelector(".dot1").style.animationIterationCount = noofiterationsofdots;
+    parent.querySelector(".dot2").style.animationIterationCount = noofiterationsofdots;
+    parent.querySelector(".dot3").style.animationIterationCount = noofiterationsofdots;
+    
             // console.log(typeof(key))
             // console.log(content[key]);
             // console.log(content);
@@ -34,9 +37,9 @@ async function hackit() {
     for (const key in content) {
    
     if (Object.prototype.hasOwnProperty.call(content, key)) {
-      let timeout=Math.round(Math.random()*10000);
+      let timeout=( Math.floor(Math.random() * (7 - 1 + 1)) + 1)*1000;//*1000 to convert to ms
 console.log(timeout);
-let noofiterationsofdots=Math.round(timeout/1000)/2;//2s for 1 iteration of dots therefore for timeout(in second) we need to divide by 2
+let noofiterationsofdots=(timeout/1000)/1;//1s for 1 iteration of dots therefore for timeout(in second) we need to divide by 1//there is a problem in this that the dots will blink for the time of current line however should blink for the time of next line but currently i am not going into it
 const element=content[key];
 
 await hacking(key,timeout,noofiterationsofdots);
